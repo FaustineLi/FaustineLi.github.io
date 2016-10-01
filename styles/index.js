89b9887e2d9af1439f96bba608430e588422a284
Faustine Li <faustineli12@gmail.com>
2016-10-01 11:22:19 -0400

    fix

--- styles/index.js
+++ styles/index.js
@@ -1,100 +1,100 @@
 var hide_lists=function(cb){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300);
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300);
 };
 
 var show_projects=function(){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300, function(){$('#projects').fadeIn(300)});
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300, function(){$('#projects').fadeIn(300)});
 	$('#projects-btn').addClass('disabled')
 };
 
 var show_posts=function(){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300, function(){$('#posts').fadeIn(300)});
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300, function(){$('#posts').fadeIn(300)});
 	$('#posts-btn').addClass('disabled')
 };
 
 var show_cv=function(){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300, function(){$('#cv').fadeIn(300)});
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300, function(){$('#cv').fadeIn(300)});
 	$('#cv-btn').addClass('disabled')
 };
 
 var show_about=function(){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300, function(){$('#about').fadeIn(300)});
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300, function(){$('#about').fadeIn(300)});
 	$('#about-btn').addClass('disabled')
 };
 
 var show_contact=function(){
 	var last=function(){
 		if($('#cv').is(':visible')) {
-			return '#cv'
+			return {div: "#cv", btn: "cv-btn"}
 		} else if ($('#about').is(':visible')) {
-			return '#about'
+			return {div: "#about", btn: "#about-btn"}
 		} else if ($('#contact').is(':visible')) {
-			return '#contact'
+			return {div: "#contact", btn: "#contact-btn"}
 		} else {
-			return '#posts'
+			return {div: "#posts", btn: "#posts-btn"}
 		}
 	}
-	$(last()).removeClass('disabled');
-	$(last()).fadeOut(300, function(){$('#contact').fadeIn(300)});
+	$(last().div).removeClass('disabled');
+	$(last().btn).fadeOut(300, function(){$('#contact').fadeIn(300)});
 	$('#contact-btn').addClass('disabled')
 };
