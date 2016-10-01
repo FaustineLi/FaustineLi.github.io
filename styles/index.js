c3b7d2062040888a43561f44ac17f992d9d9fbd2
Faustine Li <faustineli12@gmail.com>
2016-09-30 22:24:46 -0400

    more playing with js

--- styles/index.js
+++ styles/index.js
@@ -1,16 +1,43 @@
-var hide_lists=function(cb){$('#posts').fadeOut(300);
-$('#projects').fadeOut(300);$('#posts-btn').removeClass('disabled');
-$('#projects-btn').removeClass('disabled')};
+var hide_lists=function(cb){
+	$('#posts').fadeOut(300);
+	$('#projects').fadeOut(300);
+	$('#posts-btn').removeClass('disabled');
+	$('#projects-btn').removeClass('disabled')
+};
 
-var show_projects=function(){$('#posts-btn').removeClass('disabled');
-$('#posts').fadeOut(300,function(){$('#projects').fadeIn(300)});
-$('#projects-btn').addClass('disabled')};
+var show_projects=function(){
+	$('#posts-btn').removeClass('disabled');
+	$('#posts').fadeOut(300,function(){
+		$('#projects').fadeIn(300)});
+	$('#projects-btn').addClass('disabled')
+};
 
-var show_posts=function(){$('#projects-btn').removeClass('disabled');
-$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
-$('#posts-btn').addClass('disabled')};
+var show_posts=function(){
+	$('#projects-btn').removeClass('disabled');
+	$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#posts-btn').addClass('disabled')
+};
 
-var show_cv=function(){$('#projects-btn').removeClass('disabled');
-$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
-$('#cv-btn').addClass('disabled')
+var show_cv=function(){
+	$('#projects-btn').removeClass('disabled');
+	$('#posts-btn').removeClass('disabled');
+	$('#about-btn').removeClass('disabled');
+	$('#contact-btn').removeClass('disabled');
+	$('#projects').fadeOut(function(){$('#cv').fadeIn(300)});
+	$('#posts').fadeOut(function(){$('#cv').fadeIn(300)});
+	$('#about').fadeOut(function(){$('#cv').fadeIn(300)});
+	$('#contact').fadeOut(function(){$('#cv').fadeIn(300)});
+	$('#cv-btn').addClass('disabled')
+};
+
+var show_about=function(){
+	$('#projects-btn').removeClass('disabled');
+	$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#cv-btn').addClass('disabled')
+};
+
+var show_contact=function(){
+	$('#projects-btn').removeClass('disabled');
+	$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#cv-btn').addClass('disabled')
 };
