a0fc3024cb33c1bfba482c52ff6e857fa66a0511
Faustine Li <faustineli12@gmail.com>
2016-09-30 23:32:35 -0400

    add

--- styles/index.js
+++ styles/index.js
@@ -7,13 +7,25 @@ var hide_lists=function(cb){
 
 var show_projects=function(){
 	$('#posts-btn').removeClass('disabled');
+	$('#about-btn').removeClass('disabled');
+	$('#contact-btn').removeClass('disabled');
+	$('#cv-btn').removeClass('disabled');
 	$('#posts').fadeOut(300,function(){$('#projects').fadeIn(300)});
+	$('#about').fadeOut(300,function(){$('#projects').fadeIn(300)});
+	$('#cv').fadeOut(300,function(){$('#projects').fadeIn(300)});
+	$('#contact').fadeOut(300,function(){$('#projects').fadeIn(300)});
 	$('#projects-btn').addClass('disabled')
 };
 
 var show_posts=function(){
 	$('#projects-btn').removeClass('disabled');
+	$('#about-btn').removeClass('disabled');
+	$('#contact-btn').removeClass('disabled');
+	$('#cv-btn').removeClass('disabled');
 	$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#contact').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#about').fadeOut(function(){$('#posts').fadeIn(300)});
+	$('#cv').fadeOut(function(){$('#posts').fadeIn(300)});
 	$('#posts-btn').addClass('disabled')
 };
 
